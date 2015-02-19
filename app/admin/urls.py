956d33b6576dfe80c2b45cd00c59a3288137bfe4
Andrew Lapin <andrew.d.lapin@gmail.com>
2015-02-19 23:33:35 +0300

    Added login page

--- app/admin/urls.py
+++ app/admin/urls.py
@@ -1,6 +1,7 @@
 from . import admin
 
-from views import hello_world
+from views import hello_world, login
 
 
 admin.add_url_rule('/', 'hello_world', hello_world, methods=['GET'])
+admin.add_url_rule('/login', 'login', login, methods=['GET'])
--- app/admin/views.py
+++ app/admin/views.py
@@ -2,3 +2,6 @@ from flask import render_template
 
 def hello_world():
     return render_template('admin.html')
+
+def login():
+    return render_template('login.html')
