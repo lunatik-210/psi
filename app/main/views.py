be099e236076d15ea2cd3b389fecf323212a8cfd
BigBlackBug <bigblackbugg@gmail.com>
2015-02-21 00:37:08 +0300

    important news and dates are now being displayed at the homepage

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,8 @@
 from flask import render_template
 
+from app.models.models import ImportantDate, NewsItem
+
+
 def hello_world():
-    return render_template('main.html')
+    return render_template('main.html', dates=ImportantDate.objects.all(),
+                           news=NewsItem.objects.all())
