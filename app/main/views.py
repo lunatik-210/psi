0e2f4b9456a0af0e454f7f3eca1a76e09b2312c3
Andrew Lapin <andrew.d.lapin@gmail.com>
2015-02-27 01:18:28 +0300

    Added video page

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,15 @@
 from flask import render_template
 
-from app.models.models import ImportantDate, NewsItem
+from app.models.models import ImportantDate, NewsItem, Video
 
 
 def main():
-    return render_template('main.html', dates=ImportantDate.objects.all(),
-                           news=NewsItem.objects.all())
+    return render_template('main.html', 
+                            dates=ImportantDate.objects.all(),
+                            news=NewsItem.objects.all())
+
+def video():
+    return render_template('video.html', 
+                            dates=ImportantDate.objects.all(),
+                            news=NewsItem.objects.all(),
+                            videos=Video.objects.all())
