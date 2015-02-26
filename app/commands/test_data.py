7cf7c68791ac645ce361e114ef850ba4d82c8c95
Andrew Lapin <andrew.d.lapin@gmail.com>
2015-02-27 01:20:48 +0300

    Added videos to test data generation

--- app/commands/test_data.py
+++ app/commands/test_data.py
@@ -1,5 +1,5 @@
 from . import test_data
-from app.models.models import User
+from app.models.models import User, Video
 
 
 @test_data.command
@@ -8,11 +8,18 @@ def create():
     user = User(user_name="admin", password="admin")
     user.save()
 
+    videos = ['qbWUXNPf65A', 'cBsMCDgd3lg', 'KoyA0PStmls', 'AjfejT2DrJo', '0WQTgIC1Pfs', '0WQTgIC1Pfs']
+
+    for v in videos:
+        video = Video(path=v)
+        video.save()
+
 
 @test_data.command
 def delete():
     "Delete all data from database"
     User.drop_collection()
+    save.drop_collection()
 
 
 @test_data.command
