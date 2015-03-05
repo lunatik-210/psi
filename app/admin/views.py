64a7997f80ee7cc302af93b1de6ad0b0a7849875
BigBlackBug <bigblackbugg@gmail.com>
2015-03-05 11:15:57 +0300

    Video admin page now displays proper links to videos instead of plain text

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,6 +1,7 @@
 from flask import url_for, redirect, request, render_template, session
 from flask.ext.admin import expose, AdminIndexView
 from flask.ext.admin.contrib.mongoengine import ModelView
+from flask.ext.admin.model.template import macro
 from flask.ext.login import current_user, login_user, logout_user
 from wtforms import fields, widgets
 
@@ -41,7 +42,8 @@ class SpeakersView(BaseModelView):
 
 
 class VideoView(BaseModelView):
-    # column_formatters = dict(url=macro('render_price'))
+    list_template = "display_video.html"
+    column_formatters = dict(url=macro('render_url'))
     pass
 
 
@@ -57,6 +59,9 @@ class PageAdminView(BaseModelView):
 
 
 class DefaultLoginView(AdminIndexView):
+    def __init__(self):
+        super(DefaultLoginView, self).__init__(name="Authorization")
+
     @expose('/')
     def index(self):
         if current_user is not None and current_user.is_authenticated():
