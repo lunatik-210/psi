1b52bbe4ceeb937ce028051214071195ed919c92
BigBlackBug <bigblackbugg@gmail.com>
2015-03-09 22:42:29 +0300

    Cleaned up the shitty code

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,11 @@
 from collections import deque
-import datetime
-from urlparse import urlparse,parse_qs
+from urlparse import urlparse, parse_qs
+from io import BytesIO
 
-from flask import render_template, request, make_response, send_file, abort, redirect, url_for
+from flask import render_template, request, send_file, abort, redirect, url_for
 
 from app.models.models import ImportantDate, NewsItem, Video, Image, Speaker, Page
 from app import constants
-from io import BytesIO
 
 
 def get_tags(model):
@@ -21,34 +20,6 @@ def get_tags(model):
     return tags
 
 
-# def get_menu():
-#     menu = {}
-#     menu_items = {}
-#
-#     for object in Page.objects():
-#         menu_items[str(object.id)] = object
-#         if object.parent == "None":
-#             menu[str(object.id)] = []
-#
-#     for object in Page.objects():
-#         if object.parent != "None":
-#             menu[str(object.parent)].append(str(object.id))
-#
-#     for key in menu.keys():
-#         if len(menu[key]) == 0:
-#             menu[key] = None
-#
-#     return menu, menu_items
-
-
-def get_picture_tags():
-    return get_tags(Image)
-
-
-def get_video_tags():
-    return get_tags(Video)
-
-
 class Node():
     def __init__(self, value):
         self.value = value
@@ -56,6 +27,7 @@ class Node():
 
     def add_child(self, node):
         self.children.append(node)
+
 
 def reconstruct(pages):
     queue = deque(pages)
@@ -71,16 +43,20 @@ def reconstruct(pages):
         d[str(node.pk)] = new_node
     return d['None'].children
 
-def main():
+
+def get_page_data():
     locale = request.cookies.get(constants.LOCALE_TOKEN)
     data = reconstruct(Page.objects.all())
-    return render_template('main.html',
-        dates=ImportantDate.objects.all(),
-        news=NewsItem.objects.all(),
-        locale=locale,
-        video_tags=get_video_tags(),
-        data=data,
-        picture_tags=get_picture_tags())
+    return dict(dates=ImportantDate.objects.all(),
+                news=NewsItem.objects.all(),
+                locale=locale,
+                video_tags=get_tags(Video),
+                data=data,
+                picture_tags=get_tags(Image))
+
+
+def main():
+    return render_template('main.html', **get_page_data())
 
 
 def video():
@@ -92,52 +68,27 @@ def video():
     for video in videos:
         video.video_id = parse_qs(urlparse(video.url).query)['v'][0]
 
-    locale = request.cookies.get(constants.LOCALE_TOKEN)
-    data = reconstruct(Page.objects.all())
     return render_template('video.html',
-        dates=ImportantDate.objects.all(),
-        news=NewsItem.objects.all(),
-        locale=locale,
-        video_tags=get_video_tags(),
-        picture_tags=get_picture_tags(),
-        data=data,
-        videos=videos)
-
+                           videos=videos, **get_page_data())
 
 def pictures():
     tag = request.args.get('tag', None)
     images = Image.objects.all()
     if tag:
         images = Image.objects.filter(tag=tag).all()
-    locale = request.cookies.get(constants.LOCALE_TOKEN)
-    data = reconstruct(Page.objects.all())
     return render_template('pictures.html',
-        dates=ImportantDate.objects.all(),
-        news=NewsItem.objects.all(),
-        locale=locale,
-        video_tags=get_video_tags(),
-        picture_tags=get_picture_tags(),
-        data=data,
-        images=images)
+                           images=images, **get_page_data())
 
 
 def speakers():
-    locale = request.cookies.get(constants.LOCALE_TOKEN)
-    data = reconstruct(Page.objects.all())
     return render_template('speakers.html',
-        dates=ImportantDate.objects.all(),
-        news=NewsItem.objects.all(),
-        locale=locale,
-        video_tags=get_video_tags(),
-        picture_tags=get_picture_tags(),
-        data=data,
-        speakers=Speaker.objects.all())
+                           speakers=Speaker.objects.all(), **get_page_data())
 
 
 def menu():
     title = request.args.get('title', None)
 
-    if title == None:
+    if title is None:
         return redirect(url_for('main.main'))
 
     try:
@@ -145,16 +96,8 @@ def menu():
     except AttributeError:
         return redirect(url_for('main.main'))
 
-    locale = request.cookies.get(constants.LOCALE_TOKEN)
-    data = reconstruct(Page.objects.all())
     return render_template('menu_content.html',
-        dates=ImportantDate.objects.all(),
-        news=NewsItem.objects.all(),
-        locale=locale,
-        video_tags=get_video_tags(),
-        picture_tags=get_picture_tags(),
-        data=data,
-        page=page)
+                           page=page, **get_page_data())
 
 
 def images(filename=None):
